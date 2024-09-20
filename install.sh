02097164e8a4d402d1524bcb03935f2a4f09ff2f
Your Name <you@example.com>
2024-09-20 09:45:23 +0000

    Update install script to clone source code from temporary-email-service

--- install.sh
+++ install.sh
@@ -11,6 +11,9 @@ npm install -g wrangler
 # Đăng nhập vào Cloudflare
 wrangler login
 
+# Sao chép mã nguồn từ repository
+git clone https://github.com/gohcx/temporary-email-service.git temp-email-service
+cd temp-email-service
+
 # Triển khai mã nguồn lên Cloudflare Workers
-cd $(dirname "$0")
 wrangler deploy
